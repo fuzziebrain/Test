bbe2f1423923b09f56bdbd6929202c57e46f467c
DaisukeKinose <DaisukeKinose@users.noreply.github.com>
2015-01-15 16:44:53 -0800

    test Commit from Eclipse

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,7 +6,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Helloworld");
-		//changed by Daisuke Kinose by Notepad++ via GitHub for Windows
+		// changed by Daisuke Kinose by Notepad++ via GitHub for Windows
+		// changed again by Daisuke Kinose by Eclipse
+		// changed further by Notepad++ and pushed via Eclipse
+		// test
 
 	}
 
